0e91362f9a1de6c56fb58858dfbd44f3f449d2d8
yuncopy <yuncopy@sina.com>
2017-10-12 18:27:29 +0800

    后台管理

--- application/controllers/Manage.php
+++ application/controllers/Manage.php
@@ -16,35 +16,75 @@ class ManageController extends AbstractController {
         $this->assign(array('option'=>$option));
     }
     
-    // 文件上传
+    // 文件上传 图片文件
     public function uploadAction(){
         
-        //dd($_FILES);
-        
-        // 文件上传
-        $upload = new Upload_Upload(); // 实例化上传类
-        $upload->maxSize   =     3145728 ;// 设置附件上传大小
-        $upload->exts      =     array('jpg', 'gif', 'png', 'jpeg');// 设置附件上传类型
-        $upload->rootPath  =     '/home/copy/resource/'; // 设置附件上传根目录
-        // 上传文件 
-        $info   =   $upload->upload();
-        
-        
-        // 图片处理
-        $image = new Upload_Image(); 
-        $image->open('/home/copy/resource/20171012/59df200793c4f.jpg');
-        // 按照原图的比例生成一个最大为150*150的缩略图并保存为thumb.jpg
-        $image->thumb(150, 150)->save('/home/copy/resource/20171012/thumb.jpg');
-        
-        if(!$info) {// 上传错误提示错误信息
-            //dd($upload->getError());
-        }else{// 上传成功
-            dd('上传成功！');
+        if($_FILES){
+            $this->ini_set_file();
+            // 文件上传
+            $upload = new Upload_Upload(); // 实例化上传类
+            $upload->maxSize   =     3145728 ;// 设置附件上传大小
+            $upload->exts      =     array('jpg', 'gif', 'png', 'jpeg');// 设置附件上传类型
+            $upload->rootPath  =     '/home/copy/resource/'; // 设置附件上传根目录，临时设置上传目录  home/copy/resource
+            // 上传文件 
+            $info   =   $upload->upload();
+            if(!$info) {// 上传错误提示错误信息
+                //dd($upload->getError());
+            }else{// 上传成功
+                // 上传成功后移动文件，到项目目录下
+
+
+                dd('上传成功！');
+            }
         }
         
     }
     
-     // DEMO  http://document.thinkphp.cn/manual_3_2.html#image
+    public function ini_set_file(){
+        //HTTP上传文件的开关，默认为ON即是开  
+        ini_set('file_uploads','ON');
+        //通过POST、GET以及PUT方式接收数据时间进行限制为90秒 默认值：60  
+        ini_set('max_input_time','90');
+        //脚本执行时间就由默认的30秒变为180秒  
+        ini_set('max_execution_time', '180');
+        //Post变量由2M修改为8M，此值改为比upload_max_filesize要大  
+        ini_set('post_max_size', '60M');
+        //上传文件修改也为8M，和上面这个有点关系，大小不等的关系。
+        ini_set('upload_max_filesize','20M');  
+        //正在运行的脚本大量使用系统可用内存,上传图片给多点，最好比post_max_size大1.5倍  
+        ini_set('memory_limit','120M');
+    }
+
+        public function uploadseedAction(){
+            $this->ini_set_file();
+             // 文件上传
+             $upload = new Upload_Upload(); // 实例化上传类
+             $upload->maxSize   =     314572800 ;// 设置附件上传大小
+             $upload->exts      =     array('apk');// 设置附件上传类型
+             $upload->rootPath  =     '/home/copy/resource/'; // 设置附件上传根目录，临时设置上传目录
+             // 上传文件 
+             $info   =   $upload->upload();
+
+             if(!$info) {// 上传错误提示错误信息
+                 dd($upload->getError());
+             }else{// 上传成功
+                 // 上传成功后移动文件，到项目目录下
+
+                 dd($info);
+                 dd('上传成功！');
+            }
+        }
+    
+    
+
+
+
+
+
+
+
+
+    // DEMO  http://document.thinkphp.cn/manual_3_2.html#image
      public function demoAction(){
         
         //dd($_FILES);
@@ -53,9 +93,9 @@ class ManageController extends AbstractController {
         $upload = new Upload_Upload(); // 实例化上传类
         $upload->maxSize   =     3145728 ;// 设置附件上传大小
         $upload->exts      =     array('jpg', 'gif', 'png', 'jpeg');// 设置附件上传类型
-        $upload->rootPath  =     '/home/copy/resource/'; // 设置附件上传根目录
+        $upload->rootPath  =     '/home/copy/resource/'; // 设置附件上传根目录，临时设置上传目录
         // 上传文件 
-        $info   =   $upload->upload();
+        $info   =   $upload->upload();  
         
         
         // 图片处理
--- public/assets/js/siteuploader.js
+++ public/assets/js/siteuploader.js
@@ -59,6 +59,135 @@ jQuery(function() {
     // 文件上传过程中创建进度条实时显示。
     uploader.on( 'uploadProgress', function( file, percentage ) {
         var $li = $( '#'+file.id ),
+        $percent = $li.find('.progress .progress-bar');
+
+    // 避免重复创建
+    if ( !$percent.length ) {
+        $percent = $('<div class="progress progress-striped active">' +
+          '<div class="progress-bar" role="progressbar" style="width: 0%">' +
+          '</div>' +
+        '</div>').appendTo( $li ).find('.progress-bar');
+    }
+
+    $li.find('p.state').text('上传中');
+
+    $percent.css( 'width', percentage * 100 + '%' );
+});
+
+    uploader.on( 'uploadSuccess', function( file ) {
+        $( '#'+file.id ).find('p.state').text('已上传');
+    });
+
+    uploader.on( 'uploadError', function( file ) {
+         var $li = $( '#'+file.id ),
+            $error = $li.find('div.error');
+
+        // 避免重复创建
+        if ( !$error.length ) {
+            $error = $('<div class="error"></div>').appendTo( $li );
+        }
+
+        $error.text('上传失败');
+    });
+
+    uploader.on( 'uploadComplete', function( file ) {
+        $( '#'+file.id ).find('.progress').fadeOut();
+    });
+
+    uploader.on( 'all', function( type ) {
+        if ( type === 'startUpload' ) {
+            state = 'uploading';
+        } else if ( type === 'stopUpload' ) {
+            state = 'paused';
+        } else if ( type === 'uploadFinished' ) {
+            state = 'done';
+        }
+
+        if ( state === 'uploading' ) {
+            $btn.text('暂停上传');
+        } else {
+            $btn.text('开始上传');
+        }
+    });
+    
+   
+
+    $btn.on( 'click', function() {
+        if ( state === 'uploading' ) {
+            uploader.stop();
+        } else {
+            //点击提交按钮,我们这里要上传图片,以及两个text输入框的值到服务器
+            uploader.option('formData',{
+                  
+            });
+            uploader.upload();
+        }
+    });
+});
+
+
+//**********************上传文件********************
+
+;
+jQuery(function() {
+    var $ = jQuery,
+        $list = $('#thelist2'),
+        $btn = $('#ctlBtn2'),
+        state = 'pending',
+        // 优化retina, 在retina下这个值是2
+        ratio = window.devicePixelRatio || 1,
+        // 缩略图大小
+        thumbnailWidth = 100 * ratio,
+        thumbnailHeight = 100 * ratio,
+        uploader;
+
+    uploader = WebUploader.create({
+
+        // 不压缩image
+        resize: false,
+        // swf文件路径
+        swf: '/assets/Uploader.swf',
+
+        // 文件接收服务端。
+        server: '/manage/uploadseed',
+
+        // 选择文件的按钮。可选。
+        // 内部根据当前运行是创建，可能是input元素，也可能是flash.
+        pick: '#picker2',
+        // 只允许选择文件，可选。
+        accept: {
+            title: 'Images',
+            extensions: 'apk',
+            mimeTypes: 'application/*'
+        }
+    });
+
+    // 当有文件添加进来的时候
+    uploader.on( 'fileQueued', function( file ) {
+        var $li = $(
+                '<div id="' + file.id + '" class="file-item thumbnail">' +
+                    '<img>' +
+                    '<div class="info">' + file.name + '</div>' +
+                '</div>'
+                ),
+            $img = $li.find('img');
+
+        $list.append( $li );
+
+        // 创建缩略图
+        uploader.makeThumb( file, function( error, src ) {
+            if ( error ) {
+                $img.replaceWith('<span>不能预览</span>');
+                return;
+            }
+
+            $img.attr( 'src', src );
+        }, thumbnailWidth, thumbnailHeight );
+    });
+
+    // 文件上传过程中创建进度条实时显示。
+    uploader.on( 'uploadProgress', function( file, percentage ) {
+        var $li = $( '#'+file.id ),
             $percent = $li.find('.progress .progress-bar');
 
         // 避免重复创建
@@ -125,5 +254,3 @@ jQuery(function() {
     });
 });
 
-
-//**********************上传文件********************
